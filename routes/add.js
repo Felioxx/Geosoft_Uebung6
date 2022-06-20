742c7d8c65aae4b0f0be19cef25d8e0e589bb2c1
Felioxx <102677497+Felioxx@users.noreply.github.com>
2022-06-20 16:13:33 +0200

    Add files via upload

---
+++ routes/add.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+const { MongoClient } = require('mongodb')
+
+const url = 'mongodb://localhost:27017' // connection URL
+const client = new MongoClient(url) // mongodb client
+const dbName = 'mydatabase' // database name
+const collectionName = 'newpois' // collection name
+
+/* Addition home page. */
+router.get('/', function (req, res, next) {
+  res.render('add', { title: 'Addition Page' });
+});
+
+/*  */
+router.post('/newpoi', function (req, res, next) {
+  console.log("A new poi has been added through the user interface")
+
+  console.log(req.body) // show the data that has been passed through the post query
+
+  let poi = {}
+  poi.poiname = req.body.pname
+  poi.poicoords = 'hallo'
+
+
+  // gezeichnetes Polygon initialisieren
+  map.on(L.Draw.Event.CREATED, function (event) {
+    var layer = event.layer;
+    drawnItems.addLayer(layer);
+    updateText();
+  });
+  map.on("draw:edited", function (event) {
+    updateText();
+  });
+
+  var gezeichnetesPolygon = [];
+  /**
+  * generiert ein GeoJSON aus dem gezeichneten Polygon und speichert die Koordinaten in einem Array
+  * und extrahiert nach dem Zeichnen die im Polygon enthaltenen Haltestellen
+  */
+  function updateText() {
+
+    // to convert L.featureGroup to GeoJSON FeatureCollection
+    document.getElementById("geojsontextarea").value = JSON.stringify(drawnItems.toGeoJSON());
+    //console.log(drawnItems.toGeoJSON());
+    var gezeichnetesGeojson = drawnItems.toGeoJSON();
+    gezeichnetesGeojson.features.forEach(element1 => {
+      element1.geometry.coordinates.forEach(element2 => {
+        gezeichnetesPolygon.push(element2);
+      })
+    })
+    poi.poicoords = gezeichnetesPolygon
+  }
+
+  addNewPOItoDB(client, dbName, collectionName, poi, res)
+
+})
+
+
+// retrieve all elements from the database, and pass the results as input data for the search page
+async function addNewPOItoDB(client, dbName, collectionName, poi, res) {
+
+  await client.connect()
+
+  console.log('Connected successfully to server')
+
+  const db = client.db(dbName)
+
+  const collection = db.collection(collectionName)
+
+
+  collection.insertOne(poi) // see https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertOne/
+  console.log("New poi inserted in the database");
+
+  // pass the data added as input for the notification page
+  res.render('add_notification', { title: "Addition Completed", newpoi: poi })
+
+}
+
+
+module.exports = router;
